3721758b1f2559ca81049bbc4cc8e3a8cb8092f6
Noshihiko <camille.espieux@epfl.ch>
2022-03-04 14:44:44 +0100

    docu Bits et AttributeSet

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -2,16 +2,31 @@ package ch.epfl.javelo;
 
 import static ch.epfl.javelo.Preconditions.checkArgument;
 /**
- *
+ * Extrait une séquence de bits d'un vecteur de 32 bits
  *
  *  @author Camille Espieux (324248)
  *  @author Chiara Freneix (329552)
  *
  */
-
 public final class Bits {
     private Bits(){}
 
+    /**
+     * Extrait du vecteur de 32 bits value la plage de length bits (commençant au bit d'index
+     * start) qu'elle interprète comme une valeur signée en complément à deux
+     *
+     * @param value
+     *              le vecteur de 32 bits
+     * @param start
+     *              début de la plage de bits extraits
+     * @param length
+     *              plage de bits extraits
+     *
+     * @return la plage de length bits commençant au bit d'index start
+     * @throws IllegalArgumentException
+     *              si la plage est invalide, c.à.d. si elle n'est pas totalement incluse dans
+     *              l'intervalle allant de 0 à 31 (inclus)
+     */
     static int extractSigned(int value, int start, int length) {
         checkArgument((0<=length) && (0<=start) && ((length+start)<=31));
         if (length==0) return 0;
@@ -19,7 +34,22 @@ public final class Bits {
         return r >> (32-length);
     }
 
-
+    /**
+     * Extrait du vecteur de 32 bits value la plage de length bits (commençant au bit d'index
+     * start) qu'elle interprète comme une valeur non signée
+     *
+     * @param value
+     *              le vecteur de 32 bits
+     * @param start
+     *              début de la plage de bits extraits
+     * @param length
+     *              plage de bits extraits
+     *
+     * @return la plage de length bits commençant au bit d'index start
+     * @throws IllegalArgumentException
+     *              si la plage est invalide, c.à.d. si elle n'est pas totalement incluse dans
+     *              l'intervalle allant de 0 à 32 (inclus)
+     */
     static int extractUnsigned(int value, int start, int length) {
         checkArgument((0<=start) && ((length+start)<=31) && (length < 32) && (0<=length));
         if (length==0) return 0;
--- src/ch/epfl/javelo/data/AttributeSet.java
+++ src/ch/epfl/javelo/data/AttributeSet.java
@@ -5,11 +5,25 @@ import java.util.StringJoiner;
 import static ch.epfl.javelo.Preconditions.checkArgument;
 import static ch.epfl.javelo.data.Attribute.ALL;
 
+/**
+ * Représente un ensemble d'attributs OpenStreetMap
+ *
+ *  @author Camille Espieux (324248)
+ *  @author Chiara Freneix (329552)
+ *
+ */
 public record AttributeSet(long bits) {
     public AttributeSet {
         checkArgument((bits>>Attribute.COUNT) == 0);
     }
 
+    /**
+     * Construit un ensemble contenant un certain nombre d'attributs
+     *
+     * @param attributes
+     *
+     * @return Un ensemble contenant uniquement les attributs donnés en argument
+     */
     public static AttributeSet of(Attribute... attributes) {
         long seq = 0;
         for (int i=0; i< attributes.length; ++i) {
@@ -19,6 +33,16 @@ public record AttributeSet(long bits) {
         return new AttributeSet(seq);
     }
 
+    /**
+     * Permet de savoir si l'ensemble récepteur contient l'attribut donné ou non
+     *
+     * @param attribute
+     *
+     * @return vrai
+     *      ssi l'ensemble récepteur (this) contient l'attribut donné
+     * @return faux
+     *      si non
+     */
     public boolean contains(Attribute attribute) {
         boolean check;
         if (((1L << attribute.ordinal()) & this.bits)!=0) {
@@ -28,6 +52,18 @@ public record AttributeSet(long bits) {
         return check;
     }
 
+    /**
+     * Permet de savoir si l'intersection de l'ensemble récepteur avec celui passé en argument
+     * est vide ou non
+     *
+     * @param that
+     *
+     * @return vrai
+     *          ssi l'intersection de l'ensemble récepteur (this) avec celui
+     *          passé en argument (that) n'est pas vide
+     * @return faux
+     *          si non
+     */
     public boolean intersects(AttributeSet that) {
         boolean check;
         if ((that.bits & this.bits)!=0) {
@@ -37,6 +73,12 @@ public record AttributeSet(long bits) {
         return check;
     }
 
+    /**
+     * Rassemble la représentation textuelle des éléments de l'ensemble entourés d'accolades
+     * ({}) et séparés par des virgules dans un String
+     *
+     * @return une chaîne String
+     */
     public String toString() {
         StringJoiner M = new StringJoiner(",", "{", "}");
         for (int i=0; i< Attribute.COUNT; ++i) {
