ccddd479eaa12f27a0beb77cf42db2843f4ff9f1
Noshihiko <camille.espieux@epfl.ch>
2022-03-09 09:30:20 +0100

    docu sectors et node

--- src/ch/epfl/javelo/data/GraphNodes.java
+++ src/ch/epfl/javelo/data/GraphNodes.java
@@ -2,6 +2,14 @@ package ch.epfl.javelo.data;
 import ch.epfl.javelo.Bits;
 import java.nio.IntBuffer;
 
+/**
+ * Représente le tableau de tous les nœuds du graphe JaVelo
+ *
+ * @author Camille Espieux (324248)
+ * @author Chiara Freneix (329552)
+ *
+ * @param buffer
+ */
 public record GraphNodes(IntBuffer buffer) {
     //à vérifier mais normalement c'est bon
     private static final int OFFSET_E = 0;
@@ -14,21 +22,56 @@ public record GraphNodes(IntBuffer buffer) {
     private static final int debutBitIdentite = 3;
     private static final int tailleIdentite = 28;
 
+    /**
+     * @return le nombre total de noeuds
+     */
     public int count(){
         return buffer().capacity();
     }
 
+    /**
+     *
+     *
+     * @param nodeId
+     *          le nœud d'identité donné
+     * @return la coordonnée E du nœud d'identité donné
+     */
     public double nodeE(int nodeId){
         return buffer().get(NODE_INTS*nodeId+OFFSET_E);
     }
+
+    /**
+     *
+     *
+     * @param nodeId
+     *          le nœud d'identité donné
+     * @return la coordonnée N du nœud d'identité donné
+     */
     public double nodeN(int nodeId){
         return buffer().get(NODE_INTS*nodeId+OFFSET_N);
     }
 
+    /**
+     *
+     *
+     * @param nodeId
+     *          le nœud d'identité donné
+     * @return le nombre d'arêtes sortant du nœud d'identité donné
+     */
     public int outDegree(int nodeId){
         return Bits.extractUnsigned(buffer().get(NODE_INTS*nodeId+OFFSET_OUT_EDGES),debutBitAretes,tailleAretesSortantes);
     }
 
+    /**
+     * Vérifie si l'index de l'arrête est valide (compris entre 0, inclus, et le nombre d'arêtes sortant du nœud, exclus)
+     * puis donne l'identité de la edgeIndex-ième arête sortant du nœud d'identité nodeId
+     *
+     * @param nodeId
+     *          le nœud d'identité donné
+     * @param edgeIndex
+     *          index de l'arête
+     * @return l'identité de la edgeIndex-ième arête sortant du nœud d'identité nodeId
+     */
     public int edgeId(int nodeId, int edgeIndex){
         assert 0 <= edgeIndex && edgeIndex < outDegree(nodeId);
         return Bits.extractUnsigned(buffer().get(NODE_INTS*nodeId+OFFSET_OUT_EDGES),debutBitIdentite,tailleIdentite);
--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -5,11 +5,41 @@ import ch.epfl.javelo.projection.SwissBounds;
 import java.nio.ByteBuffer;
 import java.util.*;
 
+/**
+ * Représente le tableau contenant les 16384 secteurs de JaVelo
+ *
+ * @author Camille Espieux (324248)
+ * @author Chiara Freneix (329552)
+ *
+ * @param buffer
+ *         Mémoire tampon contenant la valeur des attributs de la totalité des secteurs
+ */
 public record GraphSectors(ByteBuffer buffer) {
     public static final int nbrSector = 128;
+
+    /**
+     * Représente un secteur
+     *
+     * @param startNodeId
+     *        L'identité (index) du premier nœud du secteur
+     * @param endNodeId
+     *        L'identité (index) du nœud situé juste après le dernier nœud du secteur
+     *
+     * @return un secteur
+     */
     public record Sector(int startNodeId, int endNodeId) {
     }
 
+    /**
+     * Cherche tous les secteurs contenus dans le carré centré en un point
+     *
+     * @param center
+     *        Le point donné
+     * @param distance
+     *        La distance entre le point et un des côtés du carré
+     *
+     * @return la liste de tous les secteurs ayant une intersection avec le carré centré au point donné
+     */
     public List<Sector> sectorsInArea(PointCh center, double distance) {
         //liste de sectors intersect dans le carré
         List<Sector> sectorsIntersect = new ArrayList<>();
