f51f95b4c0a6e6fd8b25235b22578e9334b414c5
chiar244 <Chiara.fr20@gmail.com>
2022-03-17 19:35:09 +0100

    grpah nikel

--- src/ch/epfl/javelo/data/Graph.java
+++ src/ch/epfl/javelo/data/Graph.java
@@ -1,24 +1,25 @@
 package ch.epfl.javelo.data;
 
+import ch.epfl.javelo.Functions;
 import ch.epfl.javelo.projection.PointCh;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.LongBuffer;
-import java.nio.ShortBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 
-import static ch.epfl.javelo.Bits.extractUnsigned;
+import static java.lang.Math.pow;
 
 public final class Graph {
 
     private static PointCh COOR;
     private static AttributeSet OSM_ATTRIBUTES;
     private static Graph graphLoadFrom;
+    private static final int OFFSET_NODE_CLOSEST=-1;
 
     public GraphNodes nodes;
     public GraphSectors sectors;
@@ -31,7 +32,6 @@ public final class Graph {
         this.edges = edges;
         this.attributeSets = attributeSets;
     }
-
 
     static Graph loadFrom(Path basePath) throws IOException{
         GraphNodes nodes = new GraphNodes((bufferFile(basePath,"nodes.bin")).asIntBuffer());
@@ -80,7 +80,21 @@ public final class Graph {
     }
 
     public int nodeClosestTo(PointCh point, double searchDistance){
+        int nodeId=OFFSET_NODE_CLOSEST;
+        double minDistance = pow(searchDistance,2);
+        List<GraphSectors.Sector> sectorsClosePoint= sectors.sectorsInArea(point, searchDistance);
 
+        for (int i =0; i<sectorsClosePoint.size();i++){
+            GraphSectors.Sector sector = sectorsClosePoint.get(i);
+
+            for (int j = sector.startNodeId(); j< sector.endNodeId() ;j++){
+                if (nodePoint(j).squaredDistanceTo(point)<=minDistance){
+                    nodeId = j;
+                    minDistance=nodePoint(j).squaredDistanceTo(point);
+                }
+            }
+        }
+        return nodeId;
     }
 
     public int edgeTargetNodeId(int edgeId){
@@ -103,14 +117,11 @@ public final class Graph {
         return edges.elevationGain(edgeId);
     }
 
-    // bien fausse sa mere, j'ai modif types et all types to protected au lieu de final
     public DoubleUnaryOperator edgeProfile(int edgeId){
-        if (!(edges.hasProfile(edgeId))) {
-            return Double.NaN;
+        if (edges.hasProfile(edgeId)) {
+            return Functions.sampled(edges.profileSamples(edgeId),edgeLength(edgeId));
         } else {
-            int profileType = extractUnsigned(profileIds.get(edgeId), 30,2);
-            return (GraphEdges.Types.ALL_types.get(profileType));
+            return Functions.constant(Double.NaN);
         }
     }
-
 }
