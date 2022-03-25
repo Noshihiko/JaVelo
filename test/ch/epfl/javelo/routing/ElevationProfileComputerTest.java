f3bca71b456066464b32fcf645f951186b2f2fa5
chiar244 <Chiara.fr20@gmail.com>
2022-03-25 09:59:57 +0100

    bohhhhh

--- test/ch/epfl/javelo/routing/ElevationProfileComputerTest.java
+++ test/ch/epfl/javelo/routing/ElevationProfileComputerTest.java
@@ -1,19 +1,23 @@
 package ch.epfl.javelo.routing;
 
+import ch.epfl.javelo.Functions;
 import ch.epfl.javelo.projection.PointCh;
 import org.junit.jupiter.api.Test;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.DoubleUnaryOperator;
 
 import static ch.epfl.javelo.routing.ElevationProfileComputer.elevationProfile;
 import static java.lang.Float.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ElevationProfileComputerTest {
     private static final double DELTA = 1e-7;
 
-    @Test
+   /* @Test
     void ElevationProfileComputerWorkOnNormalValues() {
 
         var p1 = new PointCh(2600123, 1200456);
@@ -22,7 +26,7 @@ public class ElevationProfileComputerTest {
         var p4 = new PointCh(2601000, 1201000);
 
         var edge1 = new Edge(1, 2, p1, p2, p1.distanceTo(p2), d -> 100);
-        var edge2 = new Edge(2, 3, p3, p4, p3.distanceTo(p4), d -> 100);
+
 
         var tab = new float[]{NaN, 289, 345, NaN, 367};
         var testElevationProfile = new ElevationProfile(20, tab);
@@ -33,7 +37,30 @@ public class ElevationProfileComputerTest {
 
         var testRoute = new SingleRoute(listeEdge);
 
-        assertEquals(elevationProfile(testRoute, 5), actualElevationProfile);
+        assertEquals(elevationProfile(testRoute, 5).elevationAt(1), actualElevationProfile.elevationAt(1));
+    }*/
+
+    @Test
+    void elevationProfileExceptionTest(){
+        int fromNodeId = 0;
+        int toNodeId = 10;
+        PointCh fromPoint = new PointCh(2485000, 1075000);
+        PointCh toPointCh = new PointCh(2485100, 1075100);
+        double length = 100;
+        float[] samples = {300, 310, 305, 320, 300, 290, 305, 300, 310, 300};
+        DoubleUnaryOperator a = Functions.sampled(samples, 100);
+        Edge edge = new Edge(fromNodeId, toNodeId, fromPoint, toPointCh, length, a);
+        List<Edge> edges = new ArrayList<>();
+        edges.add(edge);
+        SingleRoute route = new SingleRoute(edges);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ElevationProfile elevationProfile1 = ElevationProfileComputer.elevationProfile(route, 0);
+        } );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ElevationProfile elevationProfile1 = ElevationProfileComputer.elevationProfile(route, -1);
+        } );
     }
 
 
