323a9dad83f5d9d3209fab790a3195115aadfaf2
Noshihiko <71311782+Noshihiko@users.noreply.github.com>
2022-04-05 11:37:29 +0200

    final Q28_4

--- src/ch/epfl/javelo/Q28_4.java
+++ src/ch/epfl/javelo/Q28_4.java
@@ -3,45 +3,44 @@ package ch.epfl.javelo;
 import static java.lang.Math.scalb;
 
 /**
- * Convertit des nombres entre la représentation Q28.4 et d'autres représentations
+ * Convertit des nombres entre la représentation Q28.4 et d'autres représentations.
  *
  * @author Camille Espieux (324248)
  * @author Chiara Freneix (329552)
  */
 public final class Q28_4 {
-    private Q28_4() {
-    }
+    final static int BITS_AFTER_POINT = 4;
 
-    final static int NUMBER_OF_BITS_AFTER_POINT = 4;
+    //Constructeur privé de Q28_4 qui rend la classe non instanciable.
+    private Q28_4() {}
 
     /**
-     * Calcule la valeur Q28.4 correspondant à l'entier donné
+     * Calcule la valeur Q28.4 correspondant à l'entier donné.
      *
-     * @param i
-     * @return la valeur Q28.4
+     * @param i l'entier dont on cherche la valeur Q28.4
+     * @return la valeur Q28.4 correspondant à l'entier donné.
      */
     public static int ofInt(int i) {
-        return (i << NUMBER_OF_BITS_AFTER_POINT);
+        return (i << BITS_AFTER_POINT);
     }
 
     /**
-     * Calcule la valeur de type double égale à la valeur Q28.4 donnée
+     * Calcule la valeur de type double égale à la valeur Q28.4 donnée.
      *
-     * @param q28_4
-     * @return la valeur de type double
+     * @param q28_4 la valeur donnée dont on cherche la valeur décimale correspondante
+     * @return la valeur de type double égale à la valeur Q28.4 donnée.
      */
-
     public static double asDouble(int q28_4) {
-        return scalb((double) q28_4, -NUMBER_OF_BITS_AFTER_POINT);
+        return scalb((double) q28_4, -BITS_AFTER_POINT);
     }
 
     /**
-     * Calcule la valeur de type float égale à la valeur Q28.4 donnée
+     * Calcule la valeur de type float égale à la valeur Q28.4 donnée.
      *
-     * @param q28_4
-     * @return la valeur de type float
+     * @param q28_4 la valeur donnée dont on cherche la valeur float correspondante
+     * @return la valeur de type float égale à la valeur Q28.4 donnée.
      */
     public static float asFloat(int q28_4) {
-        return scalb(q28_4, -NUMBER_OF_BITS_AFTER_POINT);
+        return scalb(q28_4, -BITS_AFTER_POINT);
     }
 }
