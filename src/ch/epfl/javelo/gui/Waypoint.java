116ba3dadb4f32d88f76d788cad42b9473f141c5
Noshihiko <71311782+Noshihiko@users.noreply.github.com>
2022-05-28 16:17:20 +0200

    commentaire Waypoint

--- src/ch/epfl/javelo/gui/Waypoint.java
+++ src/ch/epfl/javelo/gui/Waypoint.java
@@ -2,4 +2,10 @@ package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.projection.PointCh;
 
+/**
+ * Enregistrement qui représente un point de passage.
+ *
+ * @author Camille Espieux (324248)
+ * @author Chiara Freneix (329552)
+ */
 public record Waypoint(PointCh pointCh, int nodeId){}
